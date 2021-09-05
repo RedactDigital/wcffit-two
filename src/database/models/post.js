32211d1c5b9401a6da94d2b11139b3381a2bbdfd
patrick <patrick.d.rizzardi@gmail.com>
2021-09-05 02:47:37 -0400

    added posts migration

---
+++ src/database/models/post.js
@@ -0,0 +1,46 @@
+'use strict';
+const { Model, Sequelize } = require('sequelize');
+module.exports = sequelize => {
+  class Page extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of DataTypes lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {}
+  }
+  Page.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      body: {
+        type: Sequelize.TEXT,
+      },
+      slug: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Page',
+    }
+  );
+  return Page;
+};
